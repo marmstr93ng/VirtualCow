90db7504395e65487ae9f53af8aaa14459b4cc37
Matthew Armstrong <marmstr93ng@gmai.com>
2019-04-14 02:30:42 +0100

    Add username feature where value is stored in a cookie

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import logging
 import logging.config
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, redirect
 import configparser
 
 logging.config.fileConfig('logging/log_settings.conf')
@@ -13,20 +13,36 @@ config = configparser.ConfigParser()
 def index():
     logging.info("Refreshing Index Page ...")
 
+    username = request.cookies.get('username')
     config.read(status_file_path)
 
     if request.method == "POST":
+        if request.form["action"] == "login":
+            username = request.form.get('username')
+            logging.info("Logged in with {}".format(username))
+            resp = make_response(redirect('/'))
+            resp.set_cookie('username', username)
+            return resp
+
+        if request.form["action"] == "logout":
+            resp = make_response(redirect('/'))
+            resp.delete_cookie('username')
+            return resp
+
         if request.form["action"] == "release":
             logging.info("Setting cow free status: True")
             config["Cow"]["Free"] = "True"
             with open(status_file_path, "w") as configfile:
                 config.write(configfile)
-        elif request.form["action"] == "acquire":
+
+        if request.form["action"] == "acquire":
             logging.info("Setting cow free status: False")
             config["Cow"]["Free"] = "False"
             with open(status_file_path, "w") as configfile:
                 config.write(configfile)
 
+    if username:
+        return render_template('index.html', username=username, cow_free=config["Cow"]["Free"]== "True")
     return render_template('index.html', cow_free=config["Cow"]["Free"]== "True")
 
 if __name__ == '__main__':
