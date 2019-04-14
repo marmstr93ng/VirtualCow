e6ec5a79b8255e3db24efdd6b555d38e2ca1476d
Matthew Armstrong <marmstr93ng@gmai.com>
2019-04-14 01:00:59 +0100

    Made webpage run through flask Moved stylesheet and images to static

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import logging
 import logging.config
+from flask import Flask, render_template
 
+logging.config.fileConfig('logging/log_settings.conf')
+app = Flask(__name__)
 
-def main():
-    """Function that defines the workflow of the tool"""
-
-    logging.config.fileConfig('logging/log_settings.conf')
-    logging.info("Beginning Script")
+@app.route("/")
+def index():
+    logging.info("Refreshing Index Page ...")
+    return render_template('index.html')
 
 if __name__ == '__main__':
-
-    main()
+    app.run(debug=True, host="0.0.0.0")
